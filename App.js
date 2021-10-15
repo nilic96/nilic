3f38a792684be613afa4e50693c85cb644d1135f
nilic96 <92168223+nilic96@users.noreply.github.com>
2021-10-15 21:33:17 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Alert, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Nikolina Ilić</Text>
-      <StatusBar style="auto" />
+
+
+      <View style={styles.header}>
+        <Text style={styles.title}>Kolegiji</Text>
+        <Image style={styles.tinyLogo} source={require("./assets/ffos.png")} />
+        </View>
+
+        <View style={styles.kolegiji}>
+          <TouchableOpacity style={styles.square}></TouchableOpacity>
+          <Text style={styles.kolegij}>Upravljanje znanjem</Text>
+        </View>
+
+
+        <View style={styles.kolegiji}>
+          <TouchableOpacity style={styles.square}></TouchableOpacity>
+          <Text style={styles.kolegij}>Programiranje 3</Text>
+        </View>
+
+        <View style={styles.kolegiji}>
+          <TouchableOpacity style={styles.square}></TouchableOpacity>
+          <Text style={styles.kolegij}>Dizajn korisničkog sučelja</Text>
+        </View>
+
+       
+      
+
+      <Button title="Unesi" onPress={() => Alert.alert("Button pressed")} />
+
     </View>
   );
 }
@@ -14,8 +40,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  header: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  kolegiji: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: '#25667a',
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  kolegij: {
+    fontSize: 17,
+  },
+  tinyLogo: {
+
+    width: 50,
+    height: 50,
+    marginRight: 15,
+    marginLeft: 15
+  }, 
 });
